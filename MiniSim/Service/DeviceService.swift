b8adcc17292b50623f5eda81dd16459cfe991729
Paito Anderson <pj.paito@gmail.com>
2026-09-15 20:32:36 -0400

    Remove DeviceHub simulator check

--- MiniSim/Service/DeviceService.swift
+++ MiniSim/Service/DeviceService.swift
@@ -47,7 +47,6 @@ extension DeviceServiceCommon {
       guard
         let bundleURL = app.bundleURL?.absoluteString,
         bundleURL.contains(DeviceConstants.BundleURL.simulator.rawValue) ||
-          bundleURL.contains(DeviceConstants.BundleURL.deviceHub.rawValue) ||
           bundleURL.contains(DeviceConstants.BundleURL.emulator.rawValue) else {
         continue
       }
